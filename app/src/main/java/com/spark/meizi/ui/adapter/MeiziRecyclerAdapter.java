14fb0bf185b65b508412389b185c568d98d8605c
Daniel <daniellee0929@gmail.com>
2015-12-16 22:35:38 +0800

    fix item moves when scrolling (pic is too big & fitCenter lead to some bugs)

--- app/src/main/java/com/spark/meizi/ui/adapter/MeiziRecyclerAdapter.java
+++ app/src/main/java/com/spark/meizi/ui/adapter/MeiziRecyclerAdapter.java
@@ -5,13 +5,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.spark.meizi.R;
 import com.spark.meizi.data.model.Meizi;
+import com.spark.meizi.ui.widget.RatioImageView;
 
 import java.util.List;
 
@@ -43,8 +43,10 @@ public class MeiziRecyclerAdapter extends RecyclerView.Adapter<MeiziRecyclerAdap
 
         Glide.with(context)
                 .load(meizi.getUrl())
+                .fitCenter()
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(holder.imageView);
+        holder.imageView.setOriginalSize(meizi.getWidth(),meizi.getHeight());
     }
 
     @Override
@@ -54,18 +56,13 @@ public class MeiziRecyclerAdapter extends RecyclerView.Adapter<MeiziRecyclerAdap
 
     class MeiziViewHolder extends RecyclerView.ViewHolder {
 
-        public ImageView imageView;
+        public RatioImageView imageView;
         public TextView textView;
 
         public MeiziViewHolder(View itemView) {
             super(itemView);
-            imageView = (ImageView) itemView.findViewById(R.id.riv_item);
+            imageView = (RatioImageView) itemView.findViewById(R.id.riv_item);
             textView = (TextView) itemView.findViewById(R.id.tv_item);
         }
-    }
-
-    public void replace(List<Meizi> mzs){
-        meiziList = mzs;
-        notifyDataSetChanged();
     }
 }
